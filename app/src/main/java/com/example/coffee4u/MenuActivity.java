25afcb459a1b238f71f040da8a9d3d2e1c22b1a8
Tenking <keesh-wa17@student.tarc.edu.my>
2019-01-01 23:37:21 +0800

    do layout set onclick

--- app/src/main/java/com/example/coffee4u/MenuActivity.java
+++ app/src/main/java/com/example/coffee4u/MenuActivity.java
@@ -1,14 +1,16 @@
 package com.example.coffee4u;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private LinearLayout coffeeShop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,17 +19,25 @@ public class MenuActivity extends AppCompatActivity {
         g.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.TYPE_STATUS_BAR);
         setContentView(R.layout.activity_menu);
 
+        //initialize onclick for linear layout
+        coffeeShop = findViewById(R.id.coffeeShopLay);
+        coffeeShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, DonateActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void OnClick(View view) {
         Intent intent = new Intent(MenuActivity.this, DonateActivity.class);
         startActivity(intent);
-        finish();
     }
 
     public void OnClick1(View view) {
         Intent intent = new Intent(MenuActivity.this, ContactActivity.class);
         startActivity(intent);
-        finish();
     }
 }
