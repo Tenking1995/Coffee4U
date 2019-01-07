7dfc5958459a53169eb500a277fffbfb63b8fa62
skyblastx <44184059+skyblastx@users.noreply.github.com>
2019-01-07 11:17:52 +0800

    edit intent

--- app/src/main/java/com/example/coffee4u/MenuActivity.java
+++ app/src/main/java/com/example/coffee4u/MenuActivity.java
@@ -3,6 +3,7 @@ package com.example.coffee4u;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -11,6 +12,13 @@ import android.widget.LinearLayout;
 public class MenuActivity extends AppCompatActivity {
 
     private LinearLayout coffeeShop;
+    private LinearLayout donate;
+    private LinearLayout redeemGift;
+    private LinearLayout contactUs;
+    private LinearLayout account;
+    private LinearLayout setting;
+    private LinearLayout logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,15 +28,43 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
         //initialize onclick for linear layout
-        coffeeShop = findViewById(R.id.coffeeShopLay);
-        coffeeShop.setOnClickListener(new View.OnClickListener() {
+        coffeeShop = findViewById(R.id.shop_layout);
+        donate = findViewById(R.id.donate_layout);
+        redeemGift = findViewById(R.id.redeem_layout);
+        contactUs = findViewById(R.id.contact_layout);
+        account = findViewById(R.id.account_layout);
+        setting = findViewById(R.id.setting_layout);
+        logout = findViewById(R.id.logout_layout);
+
+        //set intent for each linear layout
+//        coffeeShop.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MenuActivity.this, DonateActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+        donate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MenuActivity.this, DonateActivity.class);
                 startActivity(intent);
             }
         });
-
+        redeemGift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, GiftActivity.class);
+                startActivity(intent);
+            }
+        });
+        contactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuActivity.this, ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void OnClick(View view) {
