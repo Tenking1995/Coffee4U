099267c336f791132ba9154fc16f1c938bde9721
skyblastx <44184059+skyblastx@users.noreply.github.com>
2019-01-09 14:40:41 +0800

    Added FireStore

--- app/src/main/java/com/example/coffee4u/GiftActivity.java
+++ app/src/main/java/com/example/coffee4u/GiftActivity.java
@@ -1,16 +1,36 @@
 package com.example.coffee4u;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class GiftActivity extends AppCompatActivity {
 
     private Button btnConfirm;
     private TextView lblPoint;
     private Spinner spinnerGift;
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseFirestore mStore;
+    private ArrayList<String> arrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +41,24 @@ public class GiftActivity extends AppCompatActivity {
         btnConfirm = findViewById(R.id.confirm_button);
         lblPoint = findViewById(R.id.label_points);
         spinnerGift = findViewById(R.id.gift_spinner);
+        mStore = FirebaseFirestore.getInstance();
+
+        //initialise spinner
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, arrayList);
+        spinnerGift.setAdapter(adapter);
+
+        //get data
+        mStore.collection("coffees").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(QueryDocumentSnapshot document : task.getResult()){
+                        Map<String, Object> map = document.getData();
+                        Object giftName = map.get(map);
+                        arrayList.add(giftName.toString());
+                    }
+                }
+            }
+        });
     }
 }
